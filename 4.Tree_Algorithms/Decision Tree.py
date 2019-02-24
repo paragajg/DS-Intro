037dfb39090f5e953e3968b1ae7c6153bf52e07d
parag <paragajg@gmail.com>
2019-02-24 19:41:41 +0530

    added python file

---
+++ 4.Tree_Algorithms/Decision Tree.py
@@ -0,0 +1,516 @@
+
+# coding: utf-8
+
+# ## Decision Tree Algorithm
+
+# ### 1. Load Libraries
+
+# In[2]:
+
+
+get_ipython().system(' pip install graphviz')
+
+
+# In[ ]:
+
+
+# Data manipulation libraries
+import pandas as pd
+import numpy as np
+
+##### Scikit Learn modules needed for Logistic Regression
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn import tree 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder,MinMaxScaler , StandardScaler
+
+# Plotting libraries
+from IPython.display import SVG
+from graphviz import Source
+from IPython.display import display
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(color_codes = True)
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ### 2. Load Data
+
+# In[ ]:
+
+
+# Load data
+
+df = pd.read_csv('../3.Regression/data/winequality-red.csv')
+df.head()
+
+
+# ### 3. Preprocessing of Data
+
+# In[ ]:
+
+
+# Preprocessing of data
+# Creating 3 classes based on quality <= 4 , low; quality > 4 <= 8, medium else high
+def create_level(x):
+    # function to create levels basis wine quality
+    if x <= 5:
+        x = "low"
+    elif x > 5 and x < 7:
+        x = "medium"
+    else:
+        x = "high"
+    return x
+
+
+# In[14]:
+
+
+df['level'] = df['quality'].apply(lambda x: create_level(x))
+
+
+# In[ ]:
+
+
+print(df.quality.describe())
+print(df.level.value_counts())
+
+
+# In[ ]:
+
+
+# Encoding of categorical class variable to integers which will be used for feeding the model
+le = LabelEncoder()
+le_encoded = le.fit_transform(df['level'])
+#le_encoded = le.transform(le)
+print((le.classes_))
+print(le_encoded)
+
+
+# ### 4. Visualize Data
+
+# In[ ]:
+
+
+# Explore data visually
+# Build Correlation Matrix to
+correlation = df.iloc[:,:10].corr()
+#print(correlation)
+
+fig , ax = plt.subplots()
+fig.set_figwidth(16)
+fig.set_figheight(16)
+sns.heatmap(correlation,annot=True,cmap="YlGnBu")
+
+
+# ### 5. Split Data in Training & Testing samples
+
+# In[ ]:
+
+
+# Train & Test split
+x_train, x_test, y_train, y_test = train_test_split(df.iloc[:,:10],le_encoded,test_size=0.20,
+                                                    random_state=21)
+
+print('Shape of Training Xs:{}'.format(x_train.shape))
+print('Shape of Test Xs:{}'.format(x_test.shape))
+print('Shape of Training y:{}'.format(y_train.shape))
+print('Shape of Test y:{}'.format(y_test.shape))
+
+
+# ### 6. Build Decision Tree Model
+
+# In[77]:
+
+
+# Build Model
+clf = DecisionTreeClassifier()
+clf.fit(x_train, y_train)
+y_predicted = clf.predict(x_test)
+score=clf.score(x_test,y_test);#testing the linear regression model
+
+
+# In[ ]:
+
+
+variableImp = pd.DataFrame({"Feature":df.columns[:10],"Importance":clf.feature_importances_})
+
+fig, ax = plt.subplots()
+fig.set_figwidth(16)
+fig.set_figheight(8)
+ax.bar(x = variableImp.Feature, height = variableImp.Importance)
+#ax.barh(x = df['Gender'], height = np.mean(df.score))
+ax.set_title('Feature Importance')
+ax.set_xlabel('Feature Names')
+ax.set_ylabel('Importance')
+ax.set_ylim(top = 0.2)
+for i, v in enumerate(variableImp.Importance):
+    ax.text(i-0.4, v + 0.01, s = np.round(v,2), color='green', fontweight='bold',fontsize=14)
+
+
+# In[ ]:
+
+
+# Model diagnostic
+print(score)
+print(y_predicted)
+
+
+# In[ ]:
+
+
+df.columns[0:10]
+
+
+# In[80]:
+
+
+'''
+graph = Source(tree.export_graphviz(clf, out_file=None
+   , feature_names = df.columns[0:10], class_names = ['0', '1', '2'] 
+   , filled = True))
+
+graph.format = 'png'
+graph.render('dtree_render',view=True)
+'''
+#display(SVG(graph.pipe(format='svg')))
+
+
+# In[58]:
+
+
+tree.export_graphviz(clf,out_file='tree.dot')  
+
+
+# ### 7. Predict outcome using the Test Data
+
+# In[ ]:
+
+
+# Prediction on Test Data
+y_predicted_labels = le.inverse_transform(y_predicted)
+y_predicted_labels[0:10]
+
+
+# In[ ]:
+
+
+true_labels = le.inverse_transform(y_test)
+true_labels[0:10]
+
+
+# ### 8. Confusion Matrix
+
+# In[ ]:
+
+
+#Confusion Matrix
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(true_labels, y_predicted_labels)
+np.set_printoptions(precision=2)
+cnf_matrix
+
+
+# In[ ]:
+
+
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+# In[ ]:
+
+
+#With Normalization
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=le.classes_,
+                      title='Confusion matrix, without normalization')
+# With normalization
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes= le.classes_, normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
+
+
+# ## Model 2.0 - using MinMax Scalar to preprocess data
+
+# In[ ]:
+
+
+minmax = MinMaxScaler()
+X_minmax = minmax.fit_transform(df.iloc[:,:10])
+
+
+# In[ ]:
+
+
+X_minmax
+
+
+# In[ ]:
+
+
+# Train & Test split
+x_train, x_test, y_train, y_test = train_test_split(X_minmax,le_encoded,test_size=0.20,
+                                                    random_state=21)
+
+print('Shape of Training Xs:{}'.format(x_train.shape))
+print('Shape of Test Xs:{}'.format(x_test.shape))
+print('Shape of Training y:{}'.format(y_train.shape))
+print('Shape of Test y:{}'.format(y_test.shape))
+
+
+# In[ ]:
+
+
+# Build Model
+clf = DecisionTreeClassifier()
+clf.fit(x_train, y_train)
+y_predicted = clf.predict(x_test)
+score=clf.score(x_test,y_test);#testing the linear regression model
+
+
+# In[ ]:
+
+
+# Model diagnostic
+print(score)
+print(y_predicted)
+
+
+# In[ ]:
+
+
+# Prediction on Test Data
+y_predicted_labels = le.inverse_transform(y_predicted)
+y_predicted_labels[0:10]
+
+
+# In[ ]:
+
+
+true_labels = le.inverse_transform(y_test)
+true_labels[0:10]
+
+
+# In[ ]:
+
+
+#Confusion Matrix
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(true_labels, y_predicted_labels)
+np.set_printoptions(precision=2)
+cnf_matrix
+
+
+# In[ ]:
+
+
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+# In[ ]:
+
+
+#With Normalization
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=le.classes_,
+                      title='Confusion matrix, without normalization')
+# With normalization
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes= le.classes_, normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
+
+
+# ## Model 3.0 - using Standard Scalar to preprocess data
+
+# In[ ]:
+
+
+from sklearn.preprocessing import StandardScaler
+stdscalar = StandardScaler()
+X_stdscalar = stdscalar.fit_transform(df.iloc[:,:10])
+X_stdscalar
+
+
+# In[ ]:
+
+
+# Train & Test split
+x_train, x_test, y_train, y_test = train_test_split(X_stdscalar,le_encoded,test_size=0.20,
+                                                    random_state=21)
+
+print('Shape of Training Xs:{}'.format(x_train.shape))
+print('Shape of Test Xs:{}'.format(x_test.shape))
+print('Shape of Training y:{}'.format(y_train.shape))
+print('Shape of Test y:{}'.format(y_test.shape))
+
+
+# In[38]:
+
+
+# Build Model
+clf = DecisionTreeClassifier()
+clf.fit(x_train, y_train)
+y_predicted = clf.predict(x_test)
+score=clf.score(x_test,y_test);#testing the linear regression model
+
+
+# In[ ]:
+
+
+# Model diagnostic
+print(score)
+print(y_predicted)
+
+
+# In[ ]:
+
+
+# Prediction on Test Data
+y_predicted_labels = le.inverse_transform(y_predicted)
+y_predicted_labels[0:10]
+
+
+# In[ ]:
+
+
+true_labels = le.inverse_transform(y_test)
+true_labels[0:10]
+
+
+# In[42]:
+
+
+#Confusion Matrix
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(true_labels, y_predicted_labels)
+np.set_printoptions(precision=2)
+
+
+# In[43]:
+
+
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+# In[ ]:
+
+
+#With Normalization
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=le.classes_,
+                      title='Confusion matrix, without normalization')
+# With normalization
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes= le.classes_, normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
+
